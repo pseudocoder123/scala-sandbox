8423548a928a431b7e61c5a6d58711280f5f81b1
sakethmuthoju2k <muthojusaketh007@gmail.com>
2024-11-08 02:55:55 +0530

    day9 task kafka producer initial commit

---
+++ src/main/scala/Message.scala
@@ -0,0 +1,12 @@
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+
+
+case class Message(messageType: String, message: String, messageKey: String)
+case class ProcessMessage(message: String, messageKey: String)
+
+object JsonFormats {
+  implicit val messageFormat: RootJsonFormat[Message] = jsonFormat3(Message)
+  implicit val processMessageFormat: RootJsonFormat[ProcessMessage] = jsonFormat2(ProcessMessage)
+
+}
